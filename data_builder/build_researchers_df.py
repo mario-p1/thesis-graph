2eaa4c428bcfd495737825aa618f6f315c60a40d
Mario <84869552+mario-p1@users.noreply.github.com>
2025-11-28 20:05:59 +0100

    Add build researchers df

---
+++ data_builder/build_researchers_df.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from thesis_graph.utils import load_json_file, base_data_path
+
+
+def main():
+    profiles = load_json_file(base_data_path / "scholar_profiles.json")
+    scholar = load_json_file(base_data_path / "scholar_details.json")
+    n_articles_to_include = 10
+
+    profiles_df = pd.DataFrame(
+        profiles,
+        columns=[
+            "original_name",
+            "author_id",
+        ],
+    )
+    profiles_df = profiles_df.rename(columns={"original_name": "name"})
+
+    scholar_rows = []
+    for author_id, details in scholar.items():
+        if "interests" in details["author"]:
+            interests = [
+                interest["title"].lower() for interest in details["author"]["interests"]
+            ]
+        else:
+            interests = []
+
+        if "articles" in details:
+            articles = [article["title"] for article in details["articles"]][
+                :n_articles_to_include
+            ]
+        else:
+            articles = []
+
+        row = {
+            "author_id": author_id,
+            "interests": "|".join(interests),
+            "articles": "|".join(articles),
+        }
+
+        scholar_rows.append(row)
+
+    scholar_df = pd.DataFrame(scholar_rows)
+
+    merged_df = profiles_df.merge(scholar_df, on="author_id", how="left")
+    merged_df = merged_df.drop(columns=["author_id"])
+
+    merged_df.to_csv(base_data_path / "researchers.csv", index=False)
+    print("Saved researchers.csv")
+
+
+if __name__ == "__main__":
+    main()
